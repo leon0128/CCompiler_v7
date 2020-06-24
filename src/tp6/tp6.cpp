ddda3d51976dc8ddcff3f49b652f89c2e059b3fe
leon0128 <18aj099@ms.dendai.ac.jp>
2020-06-24 22:27:59 +0900

    [update] implements TP6::TP6::execute()

--- src/tp6/tp6.cpp
+++ src/tp6/tp6.cpp
@@ -1,1 +1,18 @@
+#include "../simbol.hpp"
 #include "tp6.hpp"
+
+bool TP6::TP6::execute(std::vector<PPToken*> &ptvec)
+{
+    for(std::size_t i = 0; i + 1 < ptvec.size(); i++)
+    {
+        if(ptvec[i]->tag == PPToken::Tag::STRING_LITERAL &&
+           ptvec[i + 1]->tag == PPToken::Tag::STRING_LITERAL)
+        {
+            ptvec[i]->uni.stringLiteral->str += ptvec[i + 1]->uni.stringLiteral->str;
+            ptvec.erase(ptvec.begin() + i + 1);
+            i--;
+        }
+    }
+
+    return true;
+}
