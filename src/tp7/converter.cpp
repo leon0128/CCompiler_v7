b364739063bdd4e2884c001ebd3695418d5da648
leon0128 <18aj099@ms.dendai.ac.jp>
2020-06-26 15:58:05 +0900

    [update] implements TP7::Converter class other than Converter::convFloatingConstant()

--- src/tp7/converter.cpp
+++ src/tp7/converter.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "../simbol.hpp"
 #include "converter.hpp"
 
@@ -12,6 +14,10 @@ bool TP7::Converter::execute(std::vector<PPToken*> &src,
             dst.push_back(token);
         else
         {
+            std::cout << "TP7 Converter error:\n"
+                         "    what: failed to convert to token from pp-token.\n"
+                         "    content: " << src[i]->string()
+                      << std::endl;
             isValid = false;
             break;
         }
@@ -22,5 +28,160 @@ bool TP7::Converter::execute(std::vector<PPToken*> &src,
 
 Token *TP7::Converter::convert(PPToken *ppToken)
 {
-    return nullptr;
+    Token retval;
+
+    if((retval.uni.punctuator = convPunctuator(ppToken)) != nullptr)
+        retval.tag = Token::Tag::PUNCTUATOR;
+    else if((retval.uni.stringLiteral = convStringLiteral(ppToken)) != nullptr)
+        retval.tag = Token::Tag::STRING_LITERAL;
+    else if((retval.uni.keyword = convKeyword(ppToken)) != nullptr)
+        retval.tag = Token::Tag::KEYWORD;
+    else if((retval.uni.identifier = convIdentifier(ppToken)) != nullptr)
+        retval.tag = Token::Tag::IDENTIFIER;
+    else if((retval.uni.constant = convConstant(ppToken)) != nullptr)
+        retval.tag = Token::Tag::CONSTANT;
+
+    return retval.tag != Token::Tag::NONE
+        ? new Token(retval)
+            : nullptr;
+}
+
+Punctuator *TP7::Converter::convPunctuator(PPToken *ppToken)
+{
+    return ppToken->tag == PPToken::Tag::PUNCTUATOR
+        ? ppToken->uni.punctuator
+            : nullptr;
+}
+
+StringLiteral *TP7::Converter::convStringLiteral(PPToken *ppToken)
+{
+    return ppToken->tag == PPToken::Tag::STRING_LITERAL
+        ? ppToken->uni.stringLiteral
+            : nullptr;
+}
+
+Keyword *TP7::Converter::convKeyword(PPToken *ppToken)
+{
+    Keyword retval;
+
+    if(ppToken->tag == PPToken::Tag::IDENTIFIER)
+    {
+        auto iter = Keyword::KEYWORD_MAP.find(ppToken->uni.identifier->str);
+        if(iter != Keyword::KEYWORD_MAP.end())
+            retval.tag = iter->second;
+    }
+
+    return retval.tag != Keyword::Tag::NONE
+        ? new Keyword(retval)
+            : nullptr;
+}
+
+Identifier *TP7::Converter::convIdentifier(PPToken *ppToken)
+{
+    return ppToken->tag == PPToken::Tag::IDENTIFIER
+        ? ppToken->uni.identifier
+            : nullptr;
+}
+
+Constant *TP7::Converter::convConstant(PPToken *ppToken)
+{
+    Constant retval;
+
+    if(ppToken->tag == PPToken::Tag::CHARACTER_CONSTANT)
+    {
+        retval.tag = Constant::Tag::CHARACTER;
+        retval.uni.character = ppToken->uni.characterConstant;
+    }
+    else if(ppToken->tag == PPToken::Tag::PP_NUMBER)
+    {
+        if((retval.uni.floating = convFloating(ppToken->uni.ppNumber->str)) != nullptr)
+            retval.tag = Constant::Tag::FLOATING;
+        else if((retval.uni.integer = convInteger(ppToken->uni.ppNumber->str)) != nullptr)
+            retval.tag = Constant::Tag::INTEGER;
+    }
+
+    return retval.tag != Constant::Tag::NONE
+        ? new Constant(retval)
+            : nullptr;
+}
+
+IntegerConstant *TP7::Converter::convInteger(const std::string &str)
+{
+    IntegerConstant retval;
+    std::size_t idx = 0;
+    bool (*isValid)(const std::string&, std::size_t) noexcept = nullptr;
+
+    // prefix
+    if(isDecimalPrefix(str, idx))
+    {
+        retval.prefixTag = IntegerConstant::PrefixTag::DECIMAL;
+        isValid = &isDecimal;
+    }
+    else if(isHexadecimalPrefix(str, idx))
+    {
+        idx += 2;
+        retval.prefixTag = IntegerConstant::PrefixTag::HEXADECIMAL;
+        isValid = &isHexadecimal;
+    }
+    else if(isOctalPrefix(str, idx))
+    {
+        idx++;
+        retval.prefixTag = IntegerConstant::PrefixTag::OCTAL;
+        isValid = &isOctal;
+    }
+    // check invalid form
+    if(retval.prefixTag == IntegerConstant::PrefixTag::NONE)
+        return nullptr;
+
+    // value
+    while(isValid(str, idx))
+        retval.str.push_back(str[idx++]);
+    // check invalid form
+    if(retval.prefixTag != IntegerConstant::PrefixTag::OCTAL &&
+       retval.str.empty())
+        return nullptr;
+    
+    // suffix
+    if(isUnsignedSuffix(str, idx))
+    {
+        idx++;
+        if(isLongLongSuffix(str, idx))
+        {
+            idx += 2;
+            retval.suffixTag = IntegerConstant::SuffixTag::UNSIGNED_LONG_LONG;
+        }
+        else if(isLongSuffix(str, idx))
+        {
+            idx++;
+            retval.suffixTag = IntegerConstant::SuffixTag::UNSIGNED_LONG;
+        }
+        else
+            retval.suffixTag = IntegerConstant::SuffixTag::UNSIGNED;
+    }
+    else if(isLongLongSuffix(str, idx))
+    {
+        idx += 2;
+        if(isUnsignedSuffix(str, idx))
+        {
+            idx++;
+            retval.suffixTag = IntegerConstant::SuffixTag::UNSIGNED_LONG_LONG;
+        }
+        else
+            retval.suffixTag = IntegerConstant::SuffixTag::LONG_LONG;
+    }
+    else if(isLongSuffix(str, idx))
+    {
+        idx++;
+        if(isUnsignedSuffix(str, idx))
+        {
+            idx++;
+            retval.suffixTag = IntegerConstant::SuffixTag::UNSIGNED_LONG;
+        }
+        else
+            retval.suffixTag = IntegerConstant::SuffixTag::LONG;
+    }
+
+    return idx == str.size()
+        ? new IntegerConstant(std::move(retval))
+            : nullptr;
 }
--- src/tp7/converter.hpp
+++ src/tp7/converter.hpp
@@ -2,9 +2,9 @@
 #define TP7_CONVERTER_HPP
 
 #include <vector>
+#include <string>
 
-class PPToken;
-class Token;
+#include "../simbol.hpp"
 
 namespace TP7
 {
@@ -17,6 +17,53 @@ public:
 
 private:
     static Token *convert(PPToken*);
+
+    static Keyword *convKeyword(PPToken*);
+    static Identifier *convIdentifier(PPToken*);
+    static Constant *convConstant(PPToken*);
+    static StringLiteral *convStringLiteral(PPToken*);
+    static Punctuator *convPunctuator(PPToken*);
+    
+    static IntegerConstant *convInteger(const std::string&);
+    static FloatingConstant *convFloating(const std::string&);
+
+// function-definition macro
+// bool (*)(const std::string&, std::size_t) noexcept
+#define IS_MATCH_CONDITION(func_name, count, conditional_expression) \
+    inline static bool func_name(const std::string &str, std::size_t idx) noexcept \
+    { \
+        return idx + (count - 1) < str.size() \
+            ? (conditional_expression) \
+                : false; \
+    }
+
+    // helper for constant
+    // prefix
+    IS_MATCH_CONDITION(isDecimalPrefix, 1,
+                       (str[idx] >= '1' && str[idx] <= '9'))
+    IS_MATCH_CONDITION(isOctalPrefix, 1,
+                       (str[idx] == '0'))
+    IS_MATCH_CONDITION(isHexadecimalPrefix, 2,
+                       (str[idx] == '0' &&
+                        (str[idx + 1] == 'x' || str[idx + 1] == 'X')))
+    // value
+    IS_MATCH_CONDITION(isDecimal, 1,
+                       (str[idx] >= '0' && str[idx] <= '9'))
+    IS_MATCH_CONDITION(isOctal, 1,
+                        (str[idx] >= '0' && str[idx] < '7'))
+    IS_MATCH_CONDITION(isHexadecimal, 1,
+                       ((str[idx] >= '0' && str[idx] <= '9') ||
+                        (str[idx] >= 'a' && str[idx] <= 'f') ||
+                        (str[idx] >= 'A' && str[idx] <= 'F')))
+    // integer-constant suffix
+    IS_MATCH_CONDITION(isUnsignedSuffix, 1,
+                       (str[idx] == 'u' || str[idx] == 'U'))
+    IS_MATCH_CONDITION(isLongSuffix, 1,
+                       (str[idx] == 'l' || str[idx] == 'L'))
+    IS_MATCH_CONDITION(isLongLongSuffix, 2,
+                       ((str[idx] == 'l' && str[idx + 1] == 'l') &&
+                        (str[idx] == 'L' && str[idx + 1] == 'L')))
+#undef IS_MATCH_CONDITION
 };
 
 }
